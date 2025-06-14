8180aedc4174516508a351c8b0585b5ec9ccbf98
Arechi <aglimbenli@gmail.com>
2025-06-14 23:44:13 +0200

    Update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import { type Metadata } from "next";
 import { Geist } from "next/font/google";
 
 export const metadata: Metadata = {
-  title: "octorel",
+  title: "OctoRel",
   description: "See latest starred GitHub repository releases",
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
