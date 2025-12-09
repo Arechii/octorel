47bd8ae10ed4c3be5468c716a0279e4fce3a37f4
Arechi <aglimbenli@gmail.com>
2025-12-09 15:40:09 +0100

    Nits

--- src/components/latest-release.tsx
+++ src/components/latest-release.tsx
@@ -32,7 +32,7 @@ export const LatestRelease = async ({
   release: Release | null;
 }) => {
   return (
-    <Card id={repository.id.toString()}>
+    <Card id={repository.id.toString()} className="max-w-min">
       <CardHeader>
         <CardTitle className="flex items-center gap-4">
           <Avatar className="size-12">
